b6dc2a68f69dde5dcdb7a0f7c5201ed5395a44a2
riponmondalbd <riponmondal149@gmail.com>
2025-11-25 21:00:29 +0600

    make auth router for auth apis

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import express from "express";
 import connectDB from "./config/mongodb.js";
+import authRouter from "./routes/authRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,8 +13,11 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ credentials: true }));
 
+// api endpoints
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
+
+app.use("/api/auth", authRouter);
 
 app.listen(port, () => console.log(`Server is running on PORT:${port}`));
