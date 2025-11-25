96ad7a30ec25e221185c55a14ab8406af19efa5e
riponmondalbd <riponmondal149@gmail.com>
2025-11-25 20:34:46 +0600

    create controller for login

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -5,6 +5,7 @@ import userModel from "../model/userModel.js";
 export const register = async (req, res) => {
   const { name, email, password } = req.body;
 
+  //   messing details
   if (!name || !email || !password) {
     return res.status(400).json({ success: false, message: "Missing details" });
   }
@@ -42,6 +43,50 @@ export const register = async (req, res) => {
       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
+
+    return res.json({ success: true });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  //   messing details
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Email and password are required" });
+  }
+
+  try {
+    const user = await userModel.findOne({ email });
+
+    // if user not exists
+    if (!user) {
+      return res.json({ success: false, message: "Invalid email" });
+    }
+
+    const isMatched = await bcrypt.compare(password, user.password);
+
+    // if password not matched
+    if (!isMatched) {
+      return res.json({ success: false, message: "Invalid password" });
+    }
+    // create token and set for cookie
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    });
+
+    return res.json({ success: true });
   } catch (error) {
     res.status(500).json({ success: false, message: error.message });
   }
