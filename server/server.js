efe2b4f1c381b7b3aa48197e6b3c1512d1187abe
riponmondalbd <riponmondal149@gmail.com>
2025-11-25 12:44:55 +0600

    connect mongodb to server

--- server/server.js
+++ server/server.js
@@ -2,9 +2,11 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import "dotenv/config";
 import express from "express";
+import connectDB from "./config/mongodb.js";
 
 const app = express();
 const port = process.env.PORT || 5000;
+connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
