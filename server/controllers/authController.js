9b7f0b9dc0d0301300aa4e400de6206e32947c34
riponmondalbd <riponmondal149@gmail.com>
2025-11-25 13:18:24 +0600

    authController --> check the values are messing or not

---
+++ server/controllers/authController.js
@@ -0,0 +1,7 @@
+export const register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    return res.status(400).json({ success: false, message: "Missing details" });
+  }
+};
