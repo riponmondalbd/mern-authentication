16142a3e4f2c8ce27822d36421fa31b79cc25952
riponmondalbd <riponmondal149@gmail.com>
2025-11-26 20:03:28 +0600

    make a controller for send otp to reset password

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -209,3 +209,41 @@ export const isAuthenticated = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
+
+// password reset otp
+export const sendResetOpt = async (req, res) => {
+  const { email } = req.body;
+
+  // email not available
+  if (!email) {
+    return res.json({ success: false, message: "Email is required" });
+  }
+
+  try {
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.json({ success: false, message: "User not found" });
+    }
+
+    //  make otp and set also expired time
+    const otp = String(Math.floor(100000 + Math.random() * 900000));
+
+    user.resetOtp = otp;
+    user.resetOtpExpiredAt = Date.now() + 15 * 60 * 1000;
+
+    await user.save();
+
+    // send otp to user
+    const mailOption = {
+      from: process.env.SENDER_EMAIL,
+      to: user.email,
+      subject: "Password Reset OPT",
+      text: `Your OTP for resetting your password is ${otp}. Use this OTP to proceed with resetting your password`,
+    };
+
+    await transporter.sendMail(mailOption);
+    return res.json({ success: true, message: "OTP sent to your email" });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
