de53d00066fb1ed4962f7ee3e874aaaa8a75b07e
riponmondalbd <riponmondal149@gmail.com>
2025-11-26 20:32:01 +0600

    make a controller for user to get user data also make api end point for user router and connect to server

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 import express from "express";
 import connectDB from "./config/mongodb.js";
 import authRouter from "./routes/authRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -19,5 +20,6 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
 app.listen(port, () => console.log(`Server is running on PORT:${port}`));
