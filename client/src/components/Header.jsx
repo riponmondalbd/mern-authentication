bccb0d2f74ca5d3403457f67bbc39d85066fdf65
riponmondalbd <riponmondal149@gmail.com>
2025-11-28 00:46:37 +0600

    show user name after login based on api call

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,10 @@
+import { useContext } from "react";
 import { assets } from "../assets/assets";
+import { AppContent } from "../context/AppContext";
 
 const Header = () => {
+  const { userData } = useContext(AppContent);
+
   return (
     <div className="flex flex-col items-center mt-20 px-4 text-center text-gray-800">
       <img
@@ -10,7 +14,7 @@ const Header = () => {
       />
 
       <h1 className="flex items-center gap-2 text-xl sm:text-3xl font-medium mb-2">
-        Hey Developer{" "}
+        Hey {userData ? userData.name : "Developer"}!{" "}
         <img
           src={assets.hand_wave}
           alt="hand wev icon"
