0e8d3db5209ddac3f95ba7b37e83151809f96f92
riponmondalbd <riponmondal149@gmail.com>
2025-11-27 23:32:32 +0600

    make login page and make ui perfectly

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,78 @@
+import { useState } from "react";
+import { assets } from "../assets/assets";
+
 const Login = () => {
+  const [state, setState] = useState("Sign Up");
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className="flex items-center justify-center min-h-screen px-6 sm:px-0 bg-linear-to-br from-blue-200 to-purple-400">
+      <img
+        src={assets.logo}
+        alt="logo"
+        className="absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer"
+      />
+
+      <div className="bg-slate-900 p-10 rounded-lg shadow-lg w-full sm:w-96 text text-indigo-300 text-sm">
+        <h2 className="text-3xl font-semibold text-white text-center mb-3">
+          {state === "Sign Up" ? "Create Account" : "Login"}
+        </h2>
+        <p className="text-center text-sm mb-6">
+          {state === "Sign Up"
+            ? "Create your account"
+            : "Login to your account!"}
+        </p>
+        <form>
+          {/* full name */}
+          <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
+            <img src={assets.person_icon} alt="person icon" />
+            <input
+              className="bg-transparent outline-none"
+              type="text"
+              placeholder="Full Name"
+              required
+            />
+          </div>
+          {/* email */}
+          <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
+            <img src={assets.mail_icon} alt="mail icon" />
+            <input
+              className="bg-transparent outline-none"
+              type="email"
+              placeholder="Email"
+              required
+            />
+          </div>
+          {/* password */}
+          <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
+            <img src={assets.lock_icon} alt="lock icon" />
+            <input
+              className="bg-transparent outline-none"
+              type="password"
+              placeholder="Password"
+              required
+            />
+          </div>
+
+          {/* forgot password */}
+          <p className="mb-4 text-indigo-500 cursor-pointer">Forgot password</p>
+
+          {/* submit button */}
+          <button className="w-full py-2.5 rounded-full bg-linear-to-r from-indigo-500 to-indigo-900 text-white font-medium">
+            {state}
+          </button>
+        </form>
+
+        <p className="text-gray-400 text-center text-xs mt-4">
+          Already have an account?{" "}
+          <span className="text-blue-400 cursor-pointer underline">Login</span>
+        </p>
+
+        <p className="text-gray-400 text-center text-xs mt-4">
+          Don't have an account?{" "}
+          <span className="text-blue-400 cursor-pointer underline">
+            Sign Up
+          </span>
+        </p>
+      </div>
     </div>
   );
 };
