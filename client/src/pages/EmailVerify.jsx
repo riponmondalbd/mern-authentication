0588f1d06d366f3baaf2d46f4d0b8b67cc8dffad
riponmondalbd <riponmondal149@gmail.com>
2025-11-28 01:59:01 +0600

    design email verify page ui

--- client/src/pages/EmailVerify.jsx
+++ client/src/pages/EmailVerify.jsx
@@ -1,7 +1,24 @@
+import { useNavigate } from "react-router";
+import { assets } from "../assets/assets";
+
 const EmailVerify = () => {
+  const navigate = useNavigate();
   return (
-    <div>
-      <h1>Email Verify</h1>
+    <div className="flex items-center justify-center min-h-screen px-6 sm:px-0 bg-linear-to-br from-blue-200 to-purple-400">
+      <img
+        onClick={() => navigate("/")}
+        src={assets.logo}
+        alt="logo"
+        className="absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer"
+      />
+      <form className="bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm">
+        <h1 className="text-white text-2xl font-semibold text-center mb-4">
+          Email Verify OTP
+        </h1>
+        <p className="text-center mb-6 text-indigo-300">
+          Enter the 6-digit code sent to your email id.
+        </p>
+      </form>
     </div>
   );
 };
