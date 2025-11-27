78362f452ca3f6ac59b2cc8932a71f7188dd11c6
riponmondalbd <riponmondal149@gmail.com>
2025-11-27 22:02:05 +0600

    make routes and connect to the page but the page have no content

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,20 @@
+import { Route, Routes } from "react-router";
+import EmailVerify from "./pages/EmailVerify";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import ResetPassword from "./pages/ResetPassword";
+
 const App = () => {
-  return <div>app</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/email-verify" element={<EmailVerify />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
---
+++ client/src/pages/EmailVerify.jsx
@@ -0,0 +1,9 @@
+const EmailVerify = () => {
+  return (
+    <div>
+      <h1>Email Verify</h1>
+    </div>
+  );
+};
+
+export default EmailVerify;
