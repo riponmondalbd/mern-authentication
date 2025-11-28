afb1138bf0e445a43f0cb325dc7c9c5f043892fd
riponmondalbd <riponmondal149@gmail.com>
2025-11-28 16:23:30 +0600

    copy page background and logo styles from another pre-made page section

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -1,7 +1,16 @@
+import { useNavigate } from "react-router";
+import { assets } from "../assets/assets";
+
 const ResetPassword = () => {
+  const navigate = useNavigate();
   return (
-    <div>
-      <h1>Reset Password</h1>
+    <div className="flex items-center justify-center min-h-screen px-6 sm:px-0 bg-linear-to-br from-blue-200 to-purple-400">
+      <img
+        onClick={() => navigate("/")}
+        src={assets.logo}
+        alt="logo"
+        className="absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer"
+      />
     </div>
   );
 };
