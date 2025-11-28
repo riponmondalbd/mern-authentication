f4afa5bf1b462f4f24e56fe4e3b832ed7cc64436
riponmondalbd <riponmondal149@gmail.com>
2025-11-28 16:44:45 +0600

    make 3 form one for send email opt one for submit opt value and the last one for new password

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -1,8 +1,39 @@
+import React, { useState } from "react";
 import { useNavigate } from "react-router";
 import { assets } from "../assets/assets";
 
 const ResetPassword = () => {
   const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+
+  const inputRefs = React.useRef([]);
+
+  // auto switch new field
+  const handleInput = (e, index) => {
+    if (e.target.value.length > 0 && index < inputRefs.current.length - 1) {
+      inputRefs.current[index + 1].focus();
+    }
+  };
+
+  // auto switch previous field when delete last one
+  const handleKeyDown = (e, index) => {
+    if (e.key === "Backspace" && e.target.value === "" && index > 0) {
+      inputRefs.current[index - 1].focus();
+    }
+  };
+
+  // pest all 6 at a time
+  const handlePest = (e) => {
+    const paste = e.clipboardData.getData("text");
+    const pastArray = paste.split("");
+    pastArray.forEach((char, index) => {
+      if (inputRefs.current[index]) {
+        inputRefs.current[index].value = char;
+      }
+    });
+  };
+
   return (
     <div className="flex items-center justify-center min-h-screen px-6 sm:px-0 bg-linear-to-br from-blue-200 to-purple-400">
       <img
@@ -24,12 +55,70 @@ const ResetPassword = () => {
           <img src={assets.mail_icon} alt="mail icon" className="w-3 h-3" />
           <input
             type="email"
-            name=""
-            id=""
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             placeholder="Email Id."
             className="bg-transparent outline-none text-white"
+            required
           />
         </div>
+        <button className="w-full py-2.5 bg-gradient-to-r from-indigo-500 to-indigo-900 text-white rounded-full mt-4">
+          Submit
+        </button>
+      </form>
+
+      {/* adding the otp */}
+      <form className="bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm">
+        <h1 className="text-white text-2xl font-semibold text-center mb-4">
+          Reset Password OTP
+        </h1>
+        <p className="text-center mb-6 text-indigo-300">
+          Enter the 6-digit code sent to your email id.
+        </p>
+
+        <div className="flex justify-between mb-8" onPaste={handlePest}>
+          {Array(6)
+            .fill(0)
+            .map((_, index) => (
+              <input
+                type="text"
+                maxLength="1"
+                key={index}
+                required
+                className="w-12 h-12 bg-[#333A5C] text-white text-center text-xl rounded-md"
+                ref={(e) => (inputRefs.current[index] = e)}
+                onInput={(e) => handleInput(e, index)}
+                onKeyDown={(e) => handleKeyDown(e, index)}
+              />
+            ))}
+        </div>
+        <button className="w-full py-2.5 bg-gradient-to-r from-indigo-500 to-indigo-900 text-white rounded-full">
+          Submit
+        </button>
+      </form>
+
+      {/* enter new password */}
+      <form className="bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm">
+        <h1 className="text-white text-2xl font-semibold text-center mb-4">
+          New password
+        </h1>
+        <p className="text-center mb-6 text-indigo-300">
+          Enter the new password
+        </p>
+        <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
+          <img src={assets.lock_icon} alt="lock icon" className="w-3 h-3" />
+          <input
+            type="password"
+            value={newPassword}
+            onChange={(e) => setNewPassword(e.target.value)}
+            placeholder="Password."
+            className="bg-transparent outline-none text-white"
+            required
+          />
+        </div>
+        <button className="w-full py-2.5 bg-gradient-to-r from-indigo-500 to-indigo-900 text-white rounded-full mt-4">
+          Submit
+        </button>
       </form>
     </div>
   );
