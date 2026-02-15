dfe50453dc22d09eaeb2969b6abc8294b47a9f3a
riponmondalbd <riponmondal149@gmail.com>
2026-02-16 03:11:43 +0600

    try to deploy

--- server/server.js
+++ server/server.js
@@ -10,7 +10,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 connectDB();
 
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://mern-authentication-orcin.vercel.app/",
+];
 
 app.use(express.json());
 app.use(cookieParser());
